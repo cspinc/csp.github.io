f6bbd58994cb1ae0713357154f6006c21b9f9164
cspinc <project@cspinc.kr>
2026-01-10 17:23:22 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CSPWhitepaper from './CSPWhitepaper';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CSPWhitepaper />
+  </React.StrictMode>
+);
